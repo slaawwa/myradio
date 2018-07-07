edbf189f3979e6632b762f0d32f139153f75735b
StanislavKovalenko <stanislav.kovalenko.dev@gmail.com>
2018-07-07 17:00:21 +0300

    - add opportunity to download current playing song (not only first as previous), - config filename - change download btn

--- index.js
+++ index.js
@@ -17,35 +17,71 @@
     let site = {
         el: {
             btn: $('#downloadBtn'),
-            parant: $parent.length? $parent: $('.soc'),
+            parent: $parent.length? $parent: $('.soc'),
         },
         style: {
-            background  : '#69abac',
-            padding     : '2px',
-            color       : '#fff',
-            fontSize    : '20px',
+            display: 'flex',
+            background: '#69abac',
+            padding: '2px',
+            color: '#fff',
+            fontSize: '20px',
+            position: 'absolute',
+            border: '1px solid #d8e4e5',
+            left: '0px',
+            width: '32px',
+            top: '-30px',
+            height: '24px',
+            'justify-content': 'center',
+            'align-items': 'center',
         },
-        tmpl: `<a href="${MRPlaylists.files[0].mp3}" id="downloadBtn" title="Завантажити">&#x1f4be;</a>`,
+        listenSongChange: () => {
+            let $currentSongNameContainer = $('.name')[ 0 ];
+            let isSongChanged = false;
+
+            $currentSongNameContainer.addEventListener( 'DOMSubtreeModified', function() {
+                isSongChanged = true;
+                ! isSongChanged && site.init();
+            } );
+        },
+        getCurrentFile: () => {
+            const currentSong = $('.playing')[ 0 ].dataset.content - 1;
+
+            return MRPlaylists.files[ currentSong ].mp3;
+        },
+        tmpl: (currentFile) => {
+            return (
+                `<a href="${ currentFile }" id="downloadBtn" title="Завантажити">
+                    <span class="download-icon"
+                    style="background: url(https://cdnradio.obozrevatel.com/img/sprite.png?v=3)
+                           -66px -58px / 656px 100px no-repeat;
+                           width: 21px;display: block;height: 17px;"></span>
+                </a>`
+            )
+        },
         download: (link, name) => {
             let a = document.createElement('a');
+
             a.href = link;
             a.download = name;
             a.click();
+
             return site;
         },
         click(e) {
+            const nameContainers = $('.player > .name > a');
+            let songName = `${ nameContainers[ 0 ].textContent } - ${ nameContainers[ 1 ].textContent } `;
+
             e.preventDefault();
-            let name = $('.ttl b').text().trim().replace(/\s+/g, ' ');
-            name = name || $('.ttl').text().trim();
-            site.download(MRPlaylists.files[0].mp3, `${name}.mp3`);
+            site.download( site.getCurrentFile(), `${ songName }.mp3`);
         },
         init: () => {
             site.el.btn.remove();
-
-            site.el.btn = $(site.tmpl)
+            const createTmpl = site.tmpl(site.getCurrentFile());
+            site.el.btn = $(createTmpl)
                 .css(site.style)
                 .click(site.click)
-                .appendTo(site.el.parant);
+                .appendTo(site.el.parent);
+            site.listenSongChange();
         },
     };
 
